e593a6cc4086c77fb002043cff03c9904b8479e9
Caroline Heymes <caroline.heymes@gmail.com>
2017-11-14 14:37:18 +0000

    pb heroku

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
    get "home/index" =>"home#index"
    get "/team" => "pages#team"
    
-  resources :products
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
